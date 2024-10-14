bdd613776f23fd81d4c72a6676899cb6fbf33f7a
unknown <abidhasanstudent20@gmail.com>
2024-10-14 20:48:34 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// pop up model
+
+
+let popup = document.querySelector('.popup');
+
+let openModal = () => {
+    popup.classList.add('popup_open');
+}
+
+let closeModal = () =>{
+    popup.classList.remove('popup_open');
+}
